c84348bd2b6e6927f005398c4a38446e35de478b
AdamELO <59090327+AdamELO@users.noreply.github.com>
2021-03-08 17:32:21 +0100

    yahtzee done

--- src/components/NextRound.js
+++ src/components/NextRound.js
@@ -2,7 +2,7 @@ function NextRound({ textModal, modalEndTurn }) {
     return (
         <div className="card">
             <h1 className='text-center'>{textModal}</h1>
-            <div className=" form__group mx-auto mt-2">
+            <div className="position-relative text-center mt-5">
                 <button onClick={modalEndTurn} className="btn colorhead mt-3">ok</button>
             </div>
         </div>
